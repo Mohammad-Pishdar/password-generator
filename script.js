302e4b8df6110d901e7a6d6fc1cef99ea37c2df3
Mo Pishdar <mohpish@gmail.com>
2020-05-31 00:01:24 +1000

    created the logic for a fucntion that gives back a random lowercase letter in our JS file

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // Assignment Code
 let generateBtn = document.querySelector('#generate');
 
+//Generator Fuctions - https://www.w3schools.com/html/html_charset.asp
+function getRandomLower () {
+  //selects a random lowercase character based on its number on charset table between and inclusive of 97 to 121 (There are 26 letters in alphabet)
+   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+
+
 // Write password to the #password input
 function writePassword() {
   let password = generatePassword();
